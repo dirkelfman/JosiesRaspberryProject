246d4caec0037f5edc31d099dcec7493e30a7fb5
Thomas Phipps <dirkelfman@gmail.com>
2015-12-30 16:56:04 -0600

    wip

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 //using System.Threading;
 using Raspberry.IO.GeneralPurpose;
 using Raspberry.IO.GeneralPurpose.Behaviors;
@@ -11,22 +12,31 @@ using Raspberry.IO.Components.Sensors.Distance.HcSr04;
 using System.Globalization;
 
 using Raspberry.Timers;
+
 namespace but2
 {
 	class MainClass
 	{
+		static SoundPlayer player;
+		static OutputPinConfiguration led1;
+		static OutputPinConfiguration led2;
+		static ProcessorPin buttonPin;
 
-		public static void Main2 ( string[] args){
+		public static void Main3 (string[] args)
+		{
 		
 			var driver = GpioConnectionSettings.DefaultDriver;
-			var led1 = ConnectorPin.P1Pin11.Output();
-			var led2 = ConnectorPin.P1Pin13.Output();
-			var butPin = ProcessorPin.Pin18;
+			led1 = ConnectorPin.P1Pin11.Output ();
+			led2 = ConnectorPin.P1Pin13.Output ();
+			buttonPin = ProcessorPin.Pin18;
 			var filename = @"/usr/share/scratch/Media/Sounds/Electronic/Whoop.wav";
-			var file = new FileStream (filename, FileMode.Open, FileAccess.Read, FileShare.Read);
-			var player = new SoundPlayer (file);
+			var stream = new MemoryStream ();
+			using (var file = new FileStream (filename, FileMode.Open, FileAccess.Read, FileShare.Read)) {
+				file.CopyTo (stream);
+			}
+			//var player = new SoundPlayer (stream);
 			var isPlaying = false;
-			driver.Allocate (ProcessorPin.Pin18, PinDirection.Input);
+			driver.Allocate (buttonPin, PinDirection.Input);
 			driver.Allocate (led1.Pin, PinDirection.Output);
 			driver.Allocate (led2.Pin, PinDirection.Output);
 
@@ -34,17 +44,19 @@ namespace but2
 			bool toggle = true;
 			while (true) {
 
-				var pressed = !driver.Read (butPin);
+				var pressed = !driver.Read (buttonPin);
 				if (pressed) { 
 					if (!isPlaying) {
-						isPlaying= true;
-						player.Stream.Position =0;
-						player.Play ();
-						//Task.Run (() => {
-							//player.Stream.Position =0;
-						//	player.PlayLooping ();
-						//	isPlaying = false;
-				//		});
+						isPlaying = true;
+						//stream.Position = 0;
+						//player.Dispose ();
+						//player.Stream.Seek (0, SeekOrigin.Begin);
+						//player.Play ();
+						Task.Run (() => {
+							player.Stream.Position = 0;
+							player.PlaySync ();
+							//	isPlaying = false;
+						});
 					}
 					driver.Write (led1.Pin, toggle);
 					toggle = !toggle;
@@ -65,40 +77,96 @@ namespace but2
 
 		}
 
-		private static void Main(string[] args)
+		private static void TooClose ()
+		{
+
+		}
+
+		private static void Main (string[] args)
 		{
 			Console.CursorVisible = false;
+
+
+			var driver = GpioConnectionSettings.DefaultDriver;
+			led1 = ConnectorPin.P1Pin11.Output ();
+			led2 = ConnectorPin.P1Pin13.Output ();
+			buttonPin = ProcessorPin.Pin18;
+			var filename = @"/usr/share/scratch/Media/Sounds/Electronic/ComputerBeeps1.wav";
+			var stream = new MemoryStream ();
+			using (var file = new FileStream (filename, FileMode.Open, FileAccess.Read, FileShare.Read)) {
+				file.CopyTo (stream);
+			}
+			stream.Position = 0;
+			//player = new SoundPlayer (filename);
+			var isPlaying = false;
+			driver.Allocate (buttonPin, PinDirection.Input);
+			driver.Allocate (led1.Pin, PinDirection.Output);
+			driver.Allocate (led2.Pin, PinDirection.Output);
+
+
+
 
 			const ConnectorPin triggerPin = ConnectorPin.P1Pin10;
 			const ConnectorPin echoPin = ConnectorPin.P1Pin08;
 
-			Console.WriteLine("HC-SR04 Sample: measure distance");
-			Console.WriteLine();
-			Console.WriteLine("\tTrigger: {0}", triggerPin);
-			Console.WriteLine("\tEcho: {0}", echoPin);
-			Console.WriteLine();
+			Console.WriteLine ("HC-SR04 Sample: measure distance");
+			Console.WriteLine ();
+			Console.WriteLine ("\tTrigger: {0}", triggerPin);
+			Console.WriteLine ("\tEcho: {0}", echoPin);
+			Console.WriteLine ();
 
-			var interval = GetInterval(args);
-			var driver = GpioConnectionSettings.DefaultDriver;
+			var interval = GetInterval (args);
+			//var driver = GpioConnectionSettings.DefaultDriver;
+			bool toggle = true;
+			using (var connection = new HcSr04Connection (
+				                        driver.Out (triggerPin.ToProcessor ()), 
+				                        driver.In (echoPin.ToProcessor ()))) {
+				while (true) {
+					try {
+						var distance = connection.GetDistance ().Centimeters;
+						if (distance < 100) {
+							if (!isPlaying) {
+								isPlaying = true;
+								Task.Run (() => {
 
-			using (var connection = new HcSr04Connection(
-				driver.Out(triggerPin.ToProcessor()), 
-				driver.In(echoPin.ToProcessor())))
-			{
-				while (!Console.KeyAvailable)
-				{
-					try
-					{
-						var distance = connection.GetDistance().Centimeters;
-						Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "{0:0.0}cm", distance).PadRight(16));
-						Console.CursorTop--;
+								
+									stream.Position = 0;
+
+									//player = new SoundPlayer(
+									lock (stream) {
+										using (player = new SoundPlayer (stream)) {
+											try {
+												player.PlaySync ();
+											} catch (Exception ex) {
+												Console.WriteLine (ex.ToString ());
+											}
+											Timer.Sleep (TimeSpan.FromSeconds (3));
+										}
+									}
+									isPlaying = false;
+								});
+							}
+							driver.Write (led1.Pin, toggle);
+							toggle = !toggle;
+							driver.Write (led2.Pin, toggle);
+						} else {
+							if (isPlaying) {
+								//lock (player) {
+								//	player.Stop ();
+								//}
+								//player.Dispose();
+								//	isPlaying = false;
+							}
+							driver.Write (led1.Pin, false);
+							driver.Write (led2.Pin, false);
+						}
+						//Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "{0:0.0}cm", distance).PadRight(16));
+						//Console.CursorTop--;
+					} catch (TimeoutException e) {
+						Console.WriteLine ("(Timeout): " + e.Message);
 					}
-					catch (TimeoutException e)
-					{
-						Console.WriteLine("(Timeout): " + e.Message);
-					}
 
-					Timer.Sleep(interval);
+					Timer.Sleep (interval);
 				}
 			}
 
@@ -107,14 +175,14 @@ namespace but2
 
 		#region Private Helpers
 
-		private static TimeSpan GetInterval(IEnumerable<string> args)
+		private static TimeSpan GetInterval (IEnumerable<string> args)
 		{
-			return TimeSpan.FromMilliseconds(args
-				.SkipWhile(a => a != "-interval")
-				.Skip(1)
-				.Select(int.Parse)
-				.DefaultIfEmpty(100)
-				.First());
+			return TimeSpan.FromMilliseconds (args
+				.SkipWhile (a => a != "-interval")
+				.Skip (1)
+				.Select (int.Parse)
+				.DefaultIfEmpty (100)
+				.First ());
 		}
 
 		#endregion
